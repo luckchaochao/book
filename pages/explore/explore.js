eb861cfc77b375d0fb26c957b8a21e7f3566f3a6
luckchaochao <54315474+luckchaochao@users.noreply.github.com>
2019-12-25 13:19:58 +0800

    探索

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -6,62 +6,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    pageType: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    let tmpThis = this
+    app.getBookList().then(res => {
+      tmpThis.setData({
+        bookList: app.globalData.bookList
+      })
+      console.log(tmpThis.data.bookList)
+    });
+    app.getAuthor().then(res => {
+      tmpThis.setData({
+        author: app.globalData.author.slice(5, 9)
+      })
+      // console.log(tmpThis.data.author)
 
+    });
+  },
+  setPageType(event) {
+    this.setData({
+      pageType: event.currentTarget.dataset.value
+    })
+    // console.log(event.currentTarget.dataset.value)
+		// console.log(this.data.pageType)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
